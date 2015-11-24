a875cbdf35ad9abec1e5f2d3e1fb52f7f7dcd6fd
Jonathan Wen <jonathanwen95@gmail.com>
2015-11-23 23:24:37 -0800

    experiment website added

--- experiment/Gruntfile.js
+++ experiment/Gruntfile.js
@@ -11,13 +11,24 @@ module.exports = function(grunt) {
         src: 'src/<%= pkg.name %>.js',
         dest: 'build/<%= pkg.name %>.min.js'
       }
+    },
+    sass: {
+      dist: {
+        options: {
+          style: 'expanded'
+        },
+        files: {
+          'main.css': 'main.scss'
+        }
+      }
     }
   });
 
   // Load the plugin that provides the "uglify" task.
   grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-sass');
 
   // Default task(s).
-  grunt.registerTask('default', ['uglify']);
+  grunt.registerTask('default', ['uglify','sass']);
 
 };
